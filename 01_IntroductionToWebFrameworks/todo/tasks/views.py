f47981d59de886dd3363246929c2a05c85d1a6d5
Lvov Stepan <lvov-styopa@ya.ru>
2020-08-27 10:33:11 +0300

    created superuser and page with random to-do list

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -2,13 +2,20 @@ from django.http import HttpResponse
 
 from django.views import View
 
+from random import choice
+
+do_list = ["Встать пораньше", "Почистить зубы", "Попить чаю", "Включить ПК", "Написать 5 надписей",
+           "Установить python", "Установить django", "Запустить сервер", "Порадоваться результату"]
+
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
+        temp_list = do_list[:]
         return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
+                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
+                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
+                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
+                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
+                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
                             '</ul>')
