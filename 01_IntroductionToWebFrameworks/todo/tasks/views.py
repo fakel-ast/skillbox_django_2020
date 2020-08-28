7500a71c67f3c4ebaaa8438d7ef1c9c3ad8f2920
Lvov Stepan <lvov-styopa@ya.ru>
2020-08-28 22:20:34 +0300

    update views.py

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,21 +1,15 @@
+from random import shuffle
+
 from django.http import HttpResponse
-
 from django.views import View
-
-from random import choice
-
-do_list = ["Встать пораньше", "Почистить зубы", "Попить чаю", "Включить ПК", "Написать 5 надписей",
-           "Установить python", "Установить django", "Запустить сервер", "Порадоваться результату"]
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        temp_list = do_list[:]
-        return HttpResponse('<ul>'
-                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
-                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
-                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
-                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
-                            f'<li>{temp_list.pop(temp_list.index(choice(temp_list)))}</li>'
-                            '</ul>')
+        do_list = ["Встать пораньше", "Почистить зубы", "Попить чаю", "Включить ПК", "Написать 5 надписей",
+                   "Установить python", "Установить django", "Запустить сервер", "Порадоваться результату"]
+        shuffle(do_list)
+        return HttpResponse("<ul>" + "".join(["<li>" + do_list.pop() + "</li>" for i in range(5)]) + "</ul>")
+
+
