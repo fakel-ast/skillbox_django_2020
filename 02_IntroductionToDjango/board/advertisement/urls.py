fed3d1cedbeaee53850b3fbdfe6cab4c0d0b7579
Lvov Stepan <lvov-styopa@ya.ru>
2020-09-15 19:01:12 +0300

    added detail course page and new def in views

--- 02_IntroductionToDjango/board/advertisement/urls.py
+++ 02_IntroductionToDjango/board/advertisement/urls.py
@@ -5,6 +5,5 @@ from .import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("<slug:id_course>/", views.course_view, name=""),
-
+    path("<slug:name_course>/", views.advertisement_course_detail_view, name="advertisement_course_detail"),
 ]
--- 02_IntroductionToDjango/board/advertisement/views.py
+++ 02_IntroductionToDjango/board/advertisement/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render
 
 
-def course_view(request, id_course, *args, **kwargs):
-    return render(request, "advertisement/" + str(id_course) + ".html")
+dict_courses = {
+    "python_course": {
+        "title": "python",
+        "text": "Курс по питону",
+        "price": "3 ханрдед бакса",
+        "img": "/media/image/python.png",
+        "link_about": "https://skillbox.ru/course/profession-python/",
+    },
+    "frontend_course": {
+        "title": "frontend",
+        "text": "Курс по фронтэнд",
+        "price": "4 ханрдед бакса",
+        "img": "/media/image/frontend.png",
+        "link_about": "https://skillbox.ru/course/profession-frontend/",
+    },
+    "java_course": {
+        "title": "java",
+        "text": "Курс по java",
+        "price": "5 ханрдед бакса",
+        "img": "/media/image/java.png",
+        "link_about": "https://skillbox.ru/course/profession-java/",
+    },
+    "web_developer_course": {
+        "title": "web_developer",
+        "text": "Курс по web разработке",
+        "price": "6 ханрдед бакса",
+        "img": "/media/image/web_developer.png",
+        "link_about": "https://skillbox.ru/course/profession-webdev/",
+    },
+    "ios_developer_course": {
+        "title": "ios_developer",
+        "text": "Курс по ios разработке",
+        "price": "7 ханрдед бакса",
+        "img": "/media/image/ios_developer.png",
+        "link_about": "https://skillbox.ru/course/profession-ios-developer/",
+    }
+}
+
+
+def advertisement_course_detail_view(request, name_course, *args, **kwargs):
+    return render(request, "advertisement/advertisement_course_detail.html",
+                  context={"course": dict_courses[name_course]})
 
 
 def home_view(request, *args, **kwargs):
-    dict_courses = [
-        {"python_course": "Курс по питону"},
-        {"java_course": "Курс по джаве"},
-        {"web_developer": "Курс по веб разработке"},
-        {"frontend": "Курс по фронтэенд разработке"},
-        {"ios_developer": "Курс по ios разработке"},
-    ]
     return render(request, "advertisement/home.html", context={"dict_courses": dict_courses})
